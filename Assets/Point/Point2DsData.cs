ca9f72ecb3105f908143a4a24b6d9ae9c22fd5b4
Bourne Young <young40@gmail.com>
2024-03-11 18:34:34 +0800

    temp

---
+++ Assets/Point/Point2DsData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point2D
+{
+    public float x;
+    public float y;
+}
+
+public class Point2DsData
+{
+    public Point2D[] points;
+}
---
+++ Assets/Point/PointClick.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointClick : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pointPrefab;
+
+    [SerializeField]
+    private GameObject pointParent;
+
+    [SerializeField]
+    private Vector2[] points;
+
+    [SerializeField]
+    private InputField filenameInput;
+
+    [SerializeField]
+    private bool isDirty = false;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (isDirty)
+        {
+            UpdatePoints();
+            isDirty = false;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            Physics.Raycast(ray, out hit);
+
+            if (hit.collider == null)
+            {
+                var go = Instantiate(pointPrefab, pointParent.transform);
+                var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                pos.z = 0;
+                go.transform.localPosition = pos;
+
+                isDirty = true;
+            }
+            else
+            {
+                if (hit.collider.name.StartsWith(pointPrefab.name))
+                {
+                    Destroy(hit.collider.gameObject);
+                    isDirty = true;
+                }
+            }
+        }
+    }
+
+    private void UpdatePoints()
+    {
+        List<Vector2> list = new List<Vector2>();
+
+        for (int i = 0; i < pointParent.transform.childCount; i++)
+        {
+            var pos = pointParent.transform.GetChild(i).position;
+
+            list.Add(pos);
+        }
+
+        points = list.ToArray();
+    }
+
+    public void SaveList()
+    {
+        UpdatePoints();
+
+        List<Point2D> list = new List<Point2D>();
+        foreach (var point in points)
+        {
+            list.Add(new Point2D { x = point.x, y = point.y });
+        }
+
+        var json = Newtonsoft.Json.JsonConvert.SerializeObject(list.ToArray(), formatting: Newtonsoft.Json.Formatting.Indented);
+
+        Debug.Log(Application.persistentDataPath);
+
+        File.WriteAllText(Path.Join(Application.persistentDataPath, filenameInput.text + ".json"), json);
+    }
+}
